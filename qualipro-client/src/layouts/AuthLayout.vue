28b1c5ed6f3e4b6c5421f26f1ebe4469cf01834e
Amen-Ammar <eamen.ammar@gmail.com>
2025-11-04 17:06:04 +0100

    add router control

--- qualipro-client/src/layouts/AuthLayout.vue
+++ qualipro-client/src/layouts/AuthLayout.vue
@@ -1,7 +1,22 @@
 <template>
-    <q-layout view="lHh Lpr lFf">
-        <q-page-container class="flex items-center justify-center bg-grey-2">
-            <slot />
+    <q-layout view="lHh Lpr lFf" class="bg-grey-2">
+        <q-page-container>
+            <q-page class="flex flex-center" style="min-height: 100vh;">
+                <q-card flat bordered class="q-pa-lg shadow-2">
+                    <slot />
+                </q-card>
+            </q-page>
         </q-page-container>
     </q-layout>
 </template>
+
+<script setup>
+// No script needed for layout wrapper
+</script>
+
+<style scoped>
+.q-card {
+    width: 400px;
+    max-width: 90vw;
+}
+</style>
--- qualipro-client/src/router/index.js
+++ qualipro-client/src/router/index.js
@@ -1,16 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../pages/LoginPage.vue'
 import UserPage from '../pages/UserPage.vue'
+import { useAuthStore } from '../stores/authStore'
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: LoginPage },
-  { path: '/users', component: UserPage }
+  {
+    path: '/users',
+    component: UserPage,
+    meta: { requiresAuth: true }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = useAuthStore()
+
+  if (!auth.token && localStorage.getItem('token')) {
+    auth.loadFromStorage()
+  }
+
+  if (to.meta.requiresAuth && !auth.token) {
+    next('/login')
+  } else if (to.path === '/login' && auth.token) {
+    next('/users')
+  } else {
+    next()
+  }
 })
 
 export default router
