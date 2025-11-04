042aff16ef542de2ea0f79bca8f5e74a83a21376
Amen-Ammar <eamen.ammar@gmail.com>
2025-11-04 16:29:20 +0100

    add missing role import

--- qualipro-server/src/services/user.service.js
+++ qualipro-server/src/services/user.service.js
@@ -1,4 +1,4 @@
-const { User } = require("../models");
+const { User, Role } = require("../models");
 const { Op } = require('sequelize');
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
